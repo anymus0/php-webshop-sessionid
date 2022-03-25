65b9f94c197353850192fcc9f757c7444090c078
Balazs Nyirfa <balazsnyirfa@gmail.com>
2022-03-25 19:55:15 +0100

    update exported sql db

--- db/php-webshop-sessionid.sql
+++ db/php-webshop-sessionid.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1:3306
--- Generation Time: Mar 25, 2022 at 03:54 PM
+-- Generation Time: Mar 25, 2022 at 06:54 PM
 -- Server version: 8.0.27
 -- PHP Version: 7.4.26
 
@@ -20,6 +20,55 @@ SET time_zone = "+00:00";
 --
 -- Database: `php-webshop-sessionid`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `cartitems`
+--
+
+DROP TABLE IF EXISTS `cartitems`;
+CREATE TABLE IF NOT EXISTS `cartitems` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `productId` int DEFAULT NULL,
+  `quantity` int NOT NULL,
+  `sessionId` varchar(255) COLLATE utf8_bin NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `productId` (`productId`),
+  KEY `sessionId` (`sessionId`)
+) ENGINE=MyISAM AUTO_INCREMENT=33 DEFAULT CHARSET=utf8mb3 COLLATE=utf8_bin;
+
+--
+-- Dumping data for table `cartitems`
+--
+
+INSERT INTO `cartitems` (`id`, `productId`, `quantity`, `sessionId`) VALUES
+(32, 7, 4, 'fm8chdsa333cdgjo5395pe1cqp'),
+(30, 5, 4, 'fm8chdsa333cdgjo5395pe1cqp');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `categories`
+--
+
+DROP TABLE IF EXISTS `categories`;
+CREATE TABLE IF NOT EXISTS `categories` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) COLLATE utf8_bin NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `name` (`name`)
+) ENGINE=MyISAM AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb3 COLLATE=utf8_bin;
+
+--
+-- Dumping data for table `categories`
+--
+
+INSERT INTO `categories` (`id`, `name`) VALUES
+(1, 'Laptop'),
+(2, 'PC'),
+(3, 'Mouse'),
+(4, 'Keyboard');
 
 -- --------------------------------------------------------
 
@@ -49,6 +98,49 @@ INSERT INTO `products` (`id`, `name`, `description`, `categoryId`) VALUES
 (5, 'Logitech G305', 'Optikai, USB', 3),
 (6, 'Cooler Master CK350 ', 'Outemu kapcsolók, LED: RGB', 4),
 (7, 'White Shark GK-2022', '50 millió garantált leütés, Mechanikus', 4);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+DROP TABLE IF EXISTS `users`;
+CREATE TABLE IF NOT EXISTS `users` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `userName` varchar(255) COLLATE utf8_bin NOT NULL,
+  `passwordHash` varchar(255) COLLATE utf8_bin NOT NULL,
+  `sessionId` varchar(255) COLLATE utf8_bin NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `userName` (`userName`),
+  UNIQUE KEY `sessionId` (`sessionId`)
+) ENGINE=MyISAM AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb3 COLLATE=utf8_bin;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `userName`, `passwordHash`, `sessionId`) VALUES
+(1, 'user', '7815696ecbf1c96e6894b779456d330e', 'fm8chdsa333cdgjo5395pe1cqp');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `usersessions`
+--
+
+DROP TABLE IF EXISTS `usersessions`;
+CREATE TABLE IF NOT EXISTS `usersessions` (
+  `id` varchar(255) COLLATE utf8_bin NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb3 COLLATE=utf8_bin;
+
+--
+-- Dumping data for table `usersessions`
+--
+
+INSERT INTO `usersessions` (`id`) VALUES
+('fm8chdsa333cdgjo5395pe1cqp');
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;

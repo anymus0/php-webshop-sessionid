17aab951482c62eb9f749b70e35cf3314c867e8a
Balazs Nyirfa <balazsnyirfa@gmail.com>
2022-03-25 13:29:07 +0100

    add index page

---
+++ src/index.php
@@ -0,0 +1,11 @@
+<?php
+include('./includes/head.php');
+?>
+
+<body>
+  <?php
+  include('./includes/header.php');
+  ?>
+
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+</body>

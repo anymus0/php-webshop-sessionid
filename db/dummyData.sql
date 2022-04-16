368cee8a3cce273dd6739512f7708fa5520ad333
Balazs Nyirfa <balazsnyirfa@gmail.com>
2022-04-16 15:42:44 +0200

    add sub-categories to data model

--- db/dummyData.sql
+++ db/dummyData.sql
@@ -9,11 +9,30 @@ INSERT INTO categories(name) VALUES ('PC');
 INSERT INTO categories(name) VALUES ('Mouse');
 INSERT INTO categories(name) VALUES ('Keyboard');
 
+-- example sub-categories
+INSERT INTO subcategories(name) VALUES ('Apple');
+INSERT INTO subcategories(name) VALUES ('Gamer');
+INSERT INTO subcategories(name) VALUES ('Budget');
+
 -- example products
-INSERT INTO products(name, description, categoryId) VALUES ('Apple MacBook Air', 'M1 Chip, 8GB RAM, 256GB SSD', '1');
-INSERT INTO products(name, description, categoryId) VALUES ('ASUS X409FA-BV643', 'Intel Core i3, 8GB RAM, 256GB SSD', '1');
-INSERT INTO products(name, description, categoryId) VALUES ('ASUS ROG Strix G15DH-HU005T', 'AMD Picasso, 8GB RAM, 512GB SSD', '2');
-INSERT INTO products(name, description, categoryId) VALUES ('Cooler Master MasterMouse MM531', 'Optikai, USB', '3');
-INSERT INTO products(name, description, categoryId) VALUES ('Logitech G305', 'Optikai, USB', '3');
-INSERT INTO products(name, description, categoryId) VALUES ('Cooler Master CK350 ', 'Outemu kapcsolók, LED: RGB', '4');
-INSERT INTO products(name, description, categoryId) VALUES ('White Shark GK-2022', '50 millió garantált leütés, Mechanikus', '4');
+INSERT INTO products(name, description, price, categoryId) VALUES ('Apple MacBook Air', 'M1 Chip, 8GB RAM, 256GB SSD', '500000', '1');
+INSERT INTO products(name, description, price, categoryId) VALUES ('ASUS X409FA-BV643', 'Intel Core i3, 8GB RAM, 256GB SSD', '200000', '1');
+INSERT INTO products(name, description, price, categoryId) VALUES ('ASUS ROG Strix G15DH-HU005T', 'AMD Picasso, 8GB RAM, 512GB SSD', '350000', '2');
+INSERT INTO products(name, description, price, categoryId) VALUES ('Cooler Master MasterMouse MM531', 'Optikai, USB', '32000', '3');
+INSERT INTO products(name, description, price, categoryId) VALUES ('Logitech G305', 'Optikai, USB', '40000', '3');
+INSERT INTO products(name, description, price, categoryId) VALUES ('Cooler Master CK350 ', 'Outemu kapcsolók, LED: RGB', '42000', '4');
+INSERT INTO products(name, description, price, categoryId) VALUES ('White Shark GK-2022', '50 millió garantált leütés, Mechanikus', '29000', '4');
+INSERT INTO products(name, description, price, categoryId) VALUES ('Apple Magic Mouse', 'Wireless', '30000', '3');
+INSERT INTO products(name, description, price, categoryId) VALUES ('HP HyperX Alloy Core', 'RGB, US layout', '9999', '4');
+
+
+INSERT INTO product_category(subCategoryId, ProductId) VALUES ('1', '1');
+INSERT INTO product_category(subCategoryId, ProductId) VALUES ('1', '8');
+INSERT INTO product_category(subCategoryId, ProductId) VALUES ('2', '3');
+INSERT INTO product_category(subCategoryId, ProductId) VALUES ('2', '4');
+INSERT INTO product_category(subCategoryId, ProductId) VALUES ('2', '5');
+INSERT INTO product_category(subCategoryId, ProductId) VALUES ('2', '6');
+INSERT INTO product_category(subCategoryId, ProductId) VALUES ('2', '7');
+INSERT INTO product_category(subCategoryId, ProductId) VALUES ('2', '9');
+INSERT INTO product_category(subCategoryId, ProductId) VALUES ('3', '2');
+INSERT INTO product_category(subCategoryId, ProductId) VALUES ('3', '9');
--- db/tables.sql
+++ db/tables.sql
@@ -15,7 +15,7 @@ CREATE TABLE users (
 	id int PRIMARY KEY NOT NULL AUTO_INCREMENT,
     userName varchar(255) NOT NULL UNIQUE,
     passwordHash varchar(255) NOT NULL,
-    sessionId varchar(255) NOT NULL UNIQUE,
+    sessionId varchar(255) NOT NULL,
     FOREIGN KEY (sessionId) REFERENCES userSessions(id)
 );
 
@@ -24,10 +24,26 @@ CREATE TABLE categories (
     name varchar(255) NOT NULL UNIQUE
 );
 
+CREATE TABLE subcategories
+(
+    id INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+    name VARCHAR(255) UNIQUE
+);
+
 CREATE TABLE products (
 	id int PRIMARY KEY NOT NULL AUTO_INCREMENT,
     name varchar(255) NOT NULL,
     description varchar(255) NOT NULL,
+    price int NOT NULL,
 	categoryId int,
     FOREIGN KEY (categoryId) REFERENCES categories(id)
+);
+
+CREATE TABLE Product_Category
+(
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    subCategoryId INT,
+    ProductId INT,
+    FOREIGN KEY (subCategoryId) REFERENCES subcategories(id),
+    FOREIGN KEY (ProductId) REFERENCES products(id)
 );
